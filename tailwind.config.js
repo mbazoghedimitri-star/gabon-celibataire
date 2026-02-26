3f58804132df74a7deece834f39e3b72f7b1bce1
mbazoghedimitri-star <mbazoghedimitri@gmail.com>
2026-02-27 00:18:55 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f4511e',
+        secondary: '#0ea5e9'
+      }
+    }
+  },
+  plugins: []
+}
